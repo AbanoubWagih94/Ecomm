1ac3bf8154df6281cb5e3667407ded1ca2b57469
abanoubwagih <abanoubwagih94@gmail.com>
2021-02-26 16:07:34 +0200

    add login system

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,15 +4,16 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 offset-sm-4">
-            <form>
+            <form method="POST" action="{{ Route('user.login') }}">
+                {{ csrf_field() }}
                 <div class="form-group">
                   <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
                   <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
+                  <input type="password" name="password" class="form-control" id="exampleInputPassword1">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
               </form>
